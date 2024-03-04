876a41e1b2d2e9f523bc515b65a2945b46cd08dd
ghiottoandrea <andreaghiotto9@gmail.com>
2024-03-04 15:31:19 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+/*
+
+BONUS 1:
+trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+organizzare i singoli membri in card/schede
+
+*/
+
+const team = [
+  {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
+  },
+  {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
+  },
+];
+
+// ## STEP
+// 1. Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// 2. Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+6
